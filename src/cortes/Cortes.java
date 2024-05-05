b0b084a292b11045063f18a198ebe2585bf8e8ae
Cortes <cortesvm1992@gmail.com>
2024-05-05 15:09:31 +0200

    Cortes.java added

---
+++ src/cortes/Cortes.java
@@ -0,0 +1,12 @@
+package cortes;
+
+public class Cortes {
+	public static void main(String[] args) {
+		imprimeNombre();
+	}
+	
+	static private void imprimeNombre() {
+		System.out.println("Hola, soy Cortes");
+	}
+	
+}
