fc17414f50019b7c028a46bac9a6d469b336a16a
María del Mar <garciajuanmar@gmail.com>
2024-05-05 14:03:38 +0200

    Hola soy Maria del Mar

---
+++ src/maria_del_mar/MariaDelMar.java
@@ -0,0 +1,10 @@
+package maria_del_mar;
+
+public class MariaDelMar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola, me llamo María del Mar");
+	}
+
+}
